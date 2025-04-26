eb7e093af95b17672c6af9a6e81392c73252ad17
Carried <ostrowski.cz@gmail.com>
2025-04-26 13:18:16 +0200

    Revert "Initial"
    This reverts commit cb2c802eb2a91239b52ffad6e3f8d60afe73b875.

--- Enka.Client/IEnkaClient.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json;
-using Enka.Client.Entities;
-
-namespace Enka.Client;
-
-public interface IEnkaClient
-{
-    public User User { get; }
-}
--- IntegrationTestConsole/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using Enka.Client;
-using Enka.Client.Entities;
-
-IEnkaClient client = EnkaProviderFactory.Create("Carried-HttpRequest-Test");
-EnkaUser user = await client.GetEnkaUserAsync("Algoinde");
-
-Console.WriteLine(user.PlayerInfo.Nickname);
